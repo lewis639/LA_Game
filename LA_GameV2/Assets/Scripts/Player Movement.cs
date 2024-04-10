a7c866006803938bf54ab29882a5706472a03499
lewis639 <ljackroyd7@gmail.com>
2024-04-10 10:28:20 +0100

    Movement + Animations

--- LA_GameV2/Assets/Scripts/Player Movement.cs
+++ LA_GameV2/Assets/Scripts/Player Movement.cs
@@ -7,7 +7,33 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
  
     private Rigidbody2D rb;
- 
+    
+    private Animator _animator;
+
+    private Vector2 _movementInput;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        SetAnimation();
+    }
+
+
+    private void SetAnimation()
+    {
+        bool IsWalkingUp = _movementInput != Vector2.zero;
+
+        _animator.SetBool("IsWalkingUp", IsWalkingUp);
+
+        bool IsWalkingDown = -_movementInput != Vector2.zero;
+
+        _animator.SetBool("IsWalkingDown", IsWalkingDown);
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
